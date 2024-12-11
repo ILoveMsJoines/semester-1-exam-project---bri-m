f11b8c3030b5adab6cb60db82bd53a8dc86d91a3
ILoveMsJoines <b.mitchell@mcaabhm.org>
2024-12-11 18:14:32 -0500

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,69 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Robert":return img`
+...bbbbbbbbbb...
+..b1111111111b..
+.b111111111111b.
+.b111111111111b.
+.bddccccccccddb.
+.bdc66666666cdb.
+.bdc61d66666cdb.
+.bdc6d666666cdb.
+.bdc66f66f66cdb.
+.bdcf666666fcdb.
+.bdc6ffffff6cdb.
+.bddccccccccddb.
+.cbbbbbbbbbbbbc.
+..cccccccccccc..
+.......cc......5
+.......cb....55.
+.......cb..5.5.5
+.......cb...5.5.
+.......cb...5...
+.......ccb......
+......cb.cb.....
+.....cb...c.....
+`;
+            case "image2":
+            case "Money Pig":return img`
+. . . . . . . . 7 . . . . . . . 
+. 5 5 5 . . 7 7 7 7 . . . . . . 
+. 5 5 5 . . 7 . 7 . . . . 5 5 5 
+. 5 5 5 . . 7 7 7 . . . . 5 5 5 
+. . 3 3 3 . . . 7 7 . 3 3 3 5 5 
+. . d d 3 . . . 7 7 . 3 d d . . 
+. . d d 3 . 7 7 7 7 . 3 d d . . 
+. . . . 3 3 3 3 7 3 3 3 . . . . 
+. . . . 3 3 3 3 3 3 3 3 . . . . 
+. . . . 3 3 f 3 3 3 3 3 . 5 5 5 
+. . . 3 3 3 3 3 3 3 3 3 . 5 5 5 
+. . . f 3 3 3 3 3 3 3 3 . 5 5 5 
+. . . 3 3 3 3 3 3 3 3 3 . 5 5 5 
+. . . . . 3 3 3 3 3 3 . 3 3 . . 
+5 5 5 . 3 3 3 3 3 3 3 3 . . . . 
+5 5 5 . f f . . . f f f . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
